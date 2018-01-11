38b8a7b969f4718d62f3e131658570405853f308
arifoyong <arifoyong@gmail.com>
2018-01-11 20:15:55 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,22 @@
+//grab dependencies
+const express = require('express'),
+				app = express(),
+				port = process.env.PORT || 8080;
+
+//configure application
+
+
+//set route
+// app.get('/', function(req,res) {
+// 	res.send('Hello World');
+// });
+
+app.get('/', (req,res) => {
+	res.send('Hello there');
+});
+
+//start our server
+app.listen(port, () => {
+	console.log(`App listening on http://localhost:${port}`);
+});
+
